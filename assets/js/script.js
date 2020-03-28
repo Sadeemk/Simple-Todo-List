83a481f26e5bf4dcca1ce98c5e4564846bd33f61
Sadeem Khan <imsadeem98@gmail.com>
2020-03-28 15:55:11 -0700

    added functionality to add and remove list items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-alert("Connected")
+// Check of specific todos by clicking
+$("li").click(function(){
+	$(this).toggleClass("completed");
+});
+
+
+//click on x to delete a todo item
+$("span").click(function(event){
+	$(this).parent().fadeOut(1000, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
