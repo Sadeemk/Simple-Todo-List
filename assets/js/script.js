4a08078559029af00534e058a9ed0c5557e3b38d
Sadeem Khan <imsadeem98@gmail.com>
2020-03-28 16:52:46 -0700

    fixed bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,24 @@
-// Check of specific todos by clicking
-$("li").click(function(){
+// Check off completed todos by clicking
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
-
 //click on x to delete a todo item
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(1000, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+//get user input from textbox to add to todo list
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		// grab new todo text from input
+		let todoText = $(this).val();
+		$(this).val("");
+
+		//create new li and add to ul
+		$("ul").append("<li><span>X </span>" + todoText + "</li>");
+	}
 });
